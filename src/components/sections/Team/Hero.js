3752ec03deb2203ce0e03ccdfbdde9dcb1e8835a
Abraham10x <amehabraham811@gmail.com>
2022-08-13 23:58:40 +0100

    update the hero section

--- src/components/sections/Team/Hero.js
+++ src/components/sections/Team/Hero.js
@@ -1,79 +1,79 @@
-import React from 'react'
-import Container from '@mui/material/Container';
-import { styled } from '@mui/material/styles';
-import Team from './../../../assets/images/section/TeamsPage/Hero-image-gradient.png'
-import { Typography } from '@mui/material';
+import React from 'react';
+import { Grid } from '@mui/material';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import { SectionProps } from '../../../utils/SectionProps';
+import Image from '../../elements/Image';
 
-const Hero = () => {
-    const StyledContainer = styled(Container)`
-        height: 50vh;
-        width: 100%; `
-    ;
-  return (
-    <React.Fragment>
-        <StyledContainer 
-          maxWidth="false" 
-          disableGutters
-          sx={{ backgroundImage: `url(${Team})`, mt: '100px' }}
-        >
-          <Container maxWidth='xl'>
-            <Typography 
-              variant='h1' 
-              sx={{
-                pt: '100px',
-                display: { xs: 'none', md: 'block' },
-                color: 'white',
-                fontFamily: 'Poppins, sans-serif',
-                fontWeight: 700,
-                fontSize: '60px',
-                lineHeight: '75px',
-              }}>
-                Find Next Place To Visit
-            </Typography>
-            <Typography 
-              variant='h1' 
-              sx={{
-                pt: '80px',
-                display: { xs: 'block', md: 'none' },
-                color: 'white',
-                fontFamily: 'Poppins, sans-serif',
-                fontWeight: 700,
-                fontSize: '50px',
-                lineHeight: '50px',
-              }}>
-                Find Next Place To Visit
-            </Typography>
-            <Typography 
-              variant='h6' 
-              sx={{
-                color: 'white',
-                display: { xs: 'none', md: 'block' },
-                fontFamily: 'Poppins, sans-serif',
-                fontWeight: 500,
-                fontSize: '20px',
-                lineHeight: '30px',
-                pb: '30px',
-              }}>
-                Discover Amazing Place With Exclusive Deals.
-            </Typography>
-            <Typography 
-              variant='h6' 
-              sx={{
-                color: 'white',
-                display: { xs: 'block', md: 'none' },
-                fontFamily: 'Poppins, sans-serif',
-                fontWeight: 500,
-                fontSize: '16px',
-                lineHeight: '20px',
-                py: '20px',
-              }}>
-                Discover Amazing Place With Exclusive Deals.
-            </Typography>
-          </Container>
-        
-        </StyledContainer>
-      </React.Fragment>
-  )
+import DottedImage from '../../../assets/images/section/TeamsPage/Hero-dotted-design.svg'
+
+const propTypes = {
+  ...SectionProps.types
 }
 
-export default Hero
+const defaultProps = {
+  ...SectionProps.defaults
+}
+
+const Hero = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'hero section center-content Hero-Banner',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'hero-inner section-inner',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container-sm">
+        <div className={innerClasses}>
+          <Grid container spacing={5}>
+            <Grid item xs={12} sm={6} md={6} style={{display: "flex", flexDirection: "column", justifyContent: "center"}} className="btns">
+              {/* <p className="m-0 reveal-from-bottom primary-color" data-reveal-delay="400"><b>OONFTs</b></p> */}
+              <h1 className="hero-h1 mt-0 mb-16 reveal-from-bottom hero-h1-banner" data-reveal-delay="200" style={{color: '#00B9BC', marginTop: '5%'}}>
+                Our Team
+              </h1>
+              <p className="m-0 mb-32 reveal-from-bottom hero-subtitle" data-reveal-delay="400">
+                We are a software & media solutions company based in Lagos, Nigeria.
+              </p>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6}>
+              <Image
+                className='hero-dotted'
+                src={DottedImage}
+                alt="Hero" 
+                />
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+          <div className='under-curve'></div>☻
+    </section>
+  );
+}
+
+Hero.propTypes = propTypes;
+Hero.defaultProps = defaultProps;
+
+export default Hero;
