c7d22bed0830e66963828d8d1f2b52dee80d9663
Abraham10x <amehabraham811@gmail.com>
2022-08-19 02:29:50 +0100

    added the bottom illustration for job application

--- src/views/JobApplication.js
+++ src/views/JobApplication.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Hero from '../components/sections/job-application/Hero'
+import JobIllustration from '../components/sections/job-application/jobIllustration'
 import LayoutDefault from '../layouts/LayoutDefault'
 
 const JobApplication = () => {
@@ -7,6 +8,7 @@ const JobApplication = () => {
     <LayoutDefault>
         <div style={{marginTop: "100px"}}>
           <Hero/>
+          <JobIllustration/>
         </div>
         
     </LayoutDefault>
