a0c1bf86f423399732fcceec73a14230a4726c71
Abraham10x <amehabraham811@gmail.com>
2022-08-20 16:46:38 +0100

    add all corrections

--- src/components/sections/general/JobPostingNewsletter.js
+++ src/components/sections/general/JobPostingNewsletter.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import { Grid, TextField, Box } from '@mui/material'
+import PrimaryButton from '../../elements/PrimaryButton'
 
 const JobPostingNewsletter = () => {
   return (
     <div style={{padding: '50px', textAlign: 'center', backgroundColor: '#2E2F6E'}}>
-        <Box>
-            <h4>Subscribe to Hashtag's Newsletter</h4>
-            <p>and be the first to get our juicy information </p>
-        </Box>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={12}>
+          <Box >
+              <h4 id='newsletter-text'>Subscribe to Hashtag's Newsletter</h4>
+              <p id='newsletter-text'>and be the first to get our juicy information </p>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12}>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1 },
+                }}
+                // className='subform-width'
+                noValidate
+                autoComplete="off"
+                >
+                <Grid container spacing={0}>
+                  <Grid item xs={12} sm={6} md={6}>
+                    <TextField size='small' className='border-white subform-width' id="outlined-basic" inputProps={{sx:{color:"white"}}} placeholder='Name' variant="outlined" />
+                  </Grid>
+                  <Grid item xs={12} sm={6} md={6}>
+                    <TextField size='small' className='border-white subform-width' id="outlined-basic" inputProps={{sx:{color:"white"}}} placeholder='Email Address' variant="outlined" />
+                    <PrimaryButton className='subscribe-btn' bgColor={'#00B9BC'} text={'Subscribe'} width={'100px'} /> 
+                  </Grid>
+                </Grid>
+            </Box>
+        </Grid>
+      </Grid>
     </div>
   )
 }
