e2f6b4b2c179b632bfe1f0189767fd5c9ce37888
sammieneutron <samson.s.samuels@gmail.com>
2022-08-31 00:38:02 +0100

    commented out contact sections

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from '../components/sections/contact/Hero'
+// import Hero from '../components/sections/contact/Hero'
 import LayoutDefault from '../layouts/LayoutDefault'
 import ContactDetail from '../components/sections/contact/ContactDetail'
 import ContactIllustration from '../components/sections/contact/ContactIllustration'
