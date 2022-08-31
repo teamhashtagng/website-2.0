6ea9d0fd75e4141232613a14bb2166cc1bd11697
sammieneutron <samson.s.samuels@gmail.com>
2022-08-31 01:29:14 +0100

    added contact section

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,16 +1,16 @@
 import React from 'react'
-// import Hero from '../components/sections/contact/Hero'
+import Hero from '../components/sections/contact/Hero'
 import LayoutDefault from '../layouts/LayoutDefault'
-// import ContactDetail from '../components/sections/contact/ContactDetail'
-// import ContactIllustration from '../components/sections/contact/ContactIllustration'
+import ContactDetail from '../components/sections/contact/ContactDetail'
+import ContactIllustration from '../components/sections/contact/ContactIllustration'
 
 const ContactUs = () => {
   return (
     <LayoutDefault>
         <div style={{marginTop: "100px"}}>
-            {/* <Hero/>
+            <Hero/>
             <ContactDetail/>
-            <ContactIllustration/> */}
+            <ContactIllustration/>
         </div>
         
     </LayoutDefault>
