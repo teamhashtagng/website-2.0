7c942ec94ebe8c49b8989333d19babc2b4ba929a
Abraham10x <amehabraham811@gmail.com>
2022-09-01 18:37:04 +0100

    done with the corrections for the about page

--- src/components/sections/about/Description.js
+++ src/components/sections/about/Description.js
@@ -5,7 +5,7 @@ const Description = () => {
   return (
     <div className='container'>
       <Box className='about-quote'>
-        <p style={{fontSize: '100px', paddingTop: '30px', paddingBottom: '-150px', position: 'relative', color: 'white'}}>"</p>
+        <p style={{fontSize: '100px', paddingTop: '30px', paddingBottom: '-150px', position: 'relative', color: 'white', marginBottom: '0'}}>"</p>
         <p style={{color: 'white'}}>We are a team of young and passionate minds. And we have a goal, which is to “make things work,” especially at home in Africa.</p>
       </Box>
     </div>
