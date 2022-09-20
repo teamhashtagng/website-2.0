ca7f9d91c1e9fbca10fc8edace4f25e9e0d2c6c6
Abraham10x <amehabraham811@gmail.com>
2022-09-20 11:28:45 +0100

    css

--- src/components/sections/career/BgImage.js
+++ src/components/sections/career/BgImage.js
@@ -1,10 +1,7 @@
-import { Container } from '@mui/material';
 import React from 'react';
-
 export default function BgImage (){
     return (
-        <div className='job-bottom-line'>
-            
+        <div className='job-bottom-line'>          
         </div>
     )
 }
