4b8750feed3d5e177581be9ef310535310f5e174
Abraham10x <amehabraham811@gmail.com>
2022-09-27 18:45:52 +0100

    Modified job posting section

--- src/components/sections/job-description/JobDetails.js
+++ src/components/sections/job-description/JobDetails.js
@@ -12,11 +12,11 @@ export default function JobDetails (){
                 <Grid container spacing={5} sx={{marginTop: '5%'}}>
                     <Grid item xs={12} sm={4} md={4}>
                         <Typography variant="body1"  gutterBottom sx={{color: '#2E2F6E', fontWeight: '700'}}>Experience</Typography>
-                        <Typography variant="body2" color={'#000000'} sx={{fontWeight: '400'}} gutterBottom>2 -3 Years</Typography>
+                        <Typography variant="body2" color={'#000000'} sx={{fontWeight: '400'}} gutterBottom>2 - 3 Years</Typography>
                     </Grid>
                     <Grid item xs={12} sm={4} md={4}>
                     <Typography variant="body1"  gutterBottom sx={{color: '#2E2F6E', fontWeight: '700'}}>Seniority Level</Typography>
-                        <Typography variant="body2" color={'#000000'} gutterBottom>Entry Level</Typography>
+                        <Typography variant="body2" color={'#000000'} gutterBottom>Junior Level</Typography>
                     </Grid>
                     <Grid item xs={12} sm={4} md={4}>
                     <Typography variant="body1"  gutterBottom sx={{color: '#2E2F6E', fontWeight: '700'}}>Employment Type</Typography>
@@ -27,7 +27,7 @@ export default function JobDetails (){
                 <Grid container spacing={4} sx={{marginTop: '3%'}}>
                     <Grid item xs={12} sm={12} md={12}>
                         <h3 className="hero-h1 mt-0 mb-16 reveal-from-bottom hero-h1-banner" data-reveal-delay="200" style={{color: '#2E2F6E'}}>
-                            Role Overview
+                            Overview
                         </h3>
                     </Grid>
                     <Grid item xs={12} sm={12} md={12}>
@@ -37,12 +37,11 @@ export default function JobDetails (){
                             alt="Hero" 
                         />
                         <p className="m-0 mb-32 reveal-from-bottom hero-subtitle" style={{color: '#000000'}} data-reveal-delay="400">
-                            We are looking for a Front-End Web Developer who is motivated to combine the art of 
-                            design with the art of programming. Responsibilities will include translation of the 
-                            UI/UX design wireframes to actual code that will produce visual elements of the 
-                            application. You will work with the UI/UX designer and bridge the gap between graphical 
-                            design and technical implementation, taking an active role on both sides and defining 
-                            how the application looks as well as how it works.
+                            Hashtag is a software & media Startup, focused on developing unique digital solutions that are
+                            relatable to a typical African.
+                            Founded in 2020, we have gradually formed a (small but) credible team, who have been working on
+                            the 'next big thing' in the world of digital solutions. While at that, we keep rooting for great talents, to
+                            come help us achieve more and be a part of the shared vision at Hashtag.
                         </p>
                     </Grid>
                 </Grid>
@@ -53,14 +52,15 @@ export default function JobDetails (){
                             Duties and Responsibilities
                         </h3>
                     </Grid>
-                    <Grid item xs={12} sm={12} md={12}>
+                    <Grid item xs={12} sm={12} md={12} sx={{zIndex: '999'}}>
                         <ul>
-                            <li>Develop new user-facing features</li>
-                            <li>Build reusable code and libraries for future use</li>
-                            <li>Ensure the technical feasibility of UI/UX designs</li>
-                            <li>Optimize application for maximum speed and scalability</li>
-                            <li>Assure that all user input is validated before submitting to back-end</li>
-                            <li>Collaborate with other team members and stakeholders</li>
+                            <li> 1 - 3 years experience of developing applications with Django and using Django-rest-framework</li>
+                            <li> Experience developing unit tests in Django and Python.</li>
+                            <li> Intermediate to advanced level knowledge of Python, Docker/containerization, git & Github.</li>
+                            <li> Should own a working laptop with modest configuration to work with.</li>
+                            <li> Must be willing to learn and keep up to date with new technology trends.</li>
+                            <li> An individual with demonstrated sense of ownership, ready to directly & indirectly contribute to the success of our Startup.</li>
+                            <li> Possesses excellent communication skills.</li>
                         </ul>
                     </Grid>
                 </Grid>
@@ -72,15 +72,10 @@ export default function JobDetails (){
                     </Grid>
                     <Grid item xs={12} sm={12} md={12}>
                         <ul>
-                            <li>Proficient understanding of web markup, including HTML5, CSS3</li>
-                            <li>Basic understanding of server-side CSS pre-processing platforms, such as LESS and SASS</li>
-                            <li>Proficient understanding of client-side scripting and JavaScript frameworks, including jQuery</li>
-                            <li>Good understanding of asynchronous request handling, partial page updates, and AJAX</li>
-                            <li>
-                                Basic knowledge of image authoring tools, to be able to crop, resize, or perform 
-                                small adjustments on an image. Familiarity with tools such as as Gimp or Photoshop 
-                                is a plus.
-                            </li>
+                            <li> A Bachelor's degree in Computer science/engineering, Systems Engineering, Mathematics or related fields of study</li>
+                            <li> Having worked in the past to deploy at least one application to Production by self.</li>
+                            <li> Sound knowledge of API documentation with Swagger & Django-rest-framework</li>
+                            <li> Recent experience working within a startup team & working remotely.</li>
                         </ul>
                     </Grid>
                     <Grid item xs={12} sm={12} md={12} sx={{ paddingBottom: '5%'}}>
